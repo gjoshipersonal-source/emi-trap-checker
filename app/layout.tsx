dbd6076deae35b7619e7ef3692261cd0d9d09380
Govind Joshi <gjoshipersonal@gmail.com>
2026-05-16 02:54:50 -0700

    fix use client placement

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,3 +35,20 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+
+
+'use client'
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl font-bold">EMI Trap Checker</h1>
+        <p className="text-center sm:text-left">
+          Scan your EMI reality
+        </p>
+      </main>
+    </div>
+  );
+}
